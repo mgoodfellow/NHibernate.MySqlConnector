8f726d78759c6f8ed84b031aa76ddd6b29bb238d
Mike Goodfellow <mike@audiu.net>
2019-10-29 15:54:40 +0000

    CHANGED: Update deps

--- src/Driver/MySqlConnectorDriver.cs
+++ src/Driver/MySqlConnectorDriver.cs
@@ -7,8 +7,8 @@ using NHibernate.Engine;
 
 namespace NHibernate.MySqlConnector.Driver
 {
-	public class MySqlConnectorDriver : DriverBase, IEmbeddedBatcherFactoryProvider
-	{
+    public class MySqlConnectorDriver : DriverBase, IEmbeddedBatcherFactoryProvider
+    {
         /// <summary>
         /// Provides a database driver for MySQL using the MySqlConnector library.
         /// </summary>
@@ -22,20 +22,20 @@ namespace NHibernate.MySqlConnector.Driver
         {
         }
 
-		/// <summary>
-		/// MySqlConnector uses named parameters in the sql.
-		/// </summary>
-		/// <value><see langword="true" /> - MySql uses <c>?</c> in the sql.</value>
-		public override bool UseNamedPrefixInSql => true;
+        /// <summary>
+        /// MySqlConnector uses named parameters in the sql.
+        /// </summary>
+        /// <value><see langword="true" /> - MySql uses <c>?</c> in the sql.</value>
+        public override bool UseNamedPrefixInSql => true;
 
-		/// <summary></summary>
-		public override bool UseNamedPrefixInParameter => true;
+        /// <summary></summary>
+        public override bool UseNamedPrefixInParameter => true;
 
-		/// <summary>
-		/// MySqlConnector use the <c>?</c> to locate parameters in sql.
-		/// </summary>
-		/// <value><c>?</c> is used to locate parameters in sql.</value>
-		public override string NamedPrefix => "?";
+        /// <summary>
+        /// MySqlConnector use the <c>?</c> to locate parameters in sql.
+        /// </summary>
+        /// <value><c>?</c> is used to locate parameters in sql.</value>
+        public override string NamedPrefix => "?";
 
         /// <summary>
         /// The MySqlConnector driver does NOT support more than 1 open DbDataReader
@@ -61,19 +61,19 @@ namespace NHibernate.MySqlConnector.Driver
         }
 
         public override IResultSetsCommand GetResultSetsCommand(ISessionImplementor session)
-		{
-			return new BasicResultSetsCommand(session);
-		}
+        {
+            return new BasicResultSetsCommand(session);
+        }
 
-		public override bool SupportsMultipleQueries => true;
+        public override bool SupportsMultipleQueries => true;
 
-		public override bool RequiresTimeSpanForTime => true;
+        public override bool RequiresTimeSpanForTime => true;
 
-		// As of v5.7, lower dates may "work" but without guarantees.
-		// https://dev.mysql.com/doc/refman/5.7/en/datetime.html
-		/// <inheritdoc />
-		public override DateTime MinDate => new DateTime(1000, 1, 1);
+        // As of v5.7, lower dates may "work" but without guarantees.
+        // https://dev.mysql.com/doc/refman/5.7/en/datetime.html
+        /// <inheritdoc />
+        public override DateTime MinDate => new DateTime(1000, 1, 1);
 
-		System.Type IEmbeddedBatcherFactoryProvider.BatcherFactoryClass => typeof(GenericBatchingBatcherFactory);
-	}
+        System.Type IEmbeddedBatcherFactoryProvider.BatcherFactoryClass => typeof(GenericBatchingBatcherFactory);
+    }
 }
